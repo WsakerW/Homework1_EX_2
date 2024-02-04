886c8144d0a0232a076afb9e6bacc263df5af6ca
WsakerW <mohammad.saker6@gmail.com>
2024-02-05 01:15:39 +0200

    This is ex1 with all the tests for partition D.

--- Traingle_Casification_class_1/src/test/java/Traingle_Casification_class_1/Traingle_Casification_class_1/AppTest.java
+++ Traingle_Casification_class_1/src/test/java/Traingle_Casification_class_1/Traingle_Casification_class_1/AppTest.java
@@ -4,33 +4,54 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-
 import static org.junit.Assert.*;
-import  org.junit.Test;
+import org.junit.Test;
 
 public class AppTest {
-	
+
 	TriangleClassifier triangle = new TriangleClassifier();
-	
+
 	@Test
-  
-	public void equilateralTriangle(){
-        int a = 2;
-        int b = 2;
-        int c=2;
-        triangle.classifyTriangle(a,b,c);
-        String expected =Triangle_Types.isosceles.toString();
-        String result = triangle.get_type().toString();
-        //assertEquals(expected, result.toString());
-    }
+	public void equilateralTriangle() {
+		int a = 2;
+		int b = 2;
+		int c = 2;
+		triangle.classifyTriangle(a, b, c);
+		String expected = Triangle_Types.equilateral.toString();
+		String result = triangle.get_type().toString();
+		assertEquals(expected, result.toString());
+	}
+
 	@Test
-	public void check_Scalene(){
-        int a = 2;
-        int b = 2;
-        int c=4;
-        triangle.classifyTriangle(a,b,c);
-        String expected =Triangle_Types.Nottriangle.toString();
-        String result = triangle.get_type().toString();
-        assertEquals(expected, result.toString());
-    }
-}  
+	public void check_Nottriangle() {
+		int a = 2;
+		int b = 2;
+		int c = 4;
+		triangle.classifyTriangle(a, b, c);
+		String expected = Triangle_Types.Nottriangle.toString();
+		String result = triangle.get_type().toString();
+		assertEquals(expected, result.toString());
+	}
+
+	@Test
+	public void check_Scalene() {
+		int a = 2;
+		int b = 3;
+		int c = 4;
+		triangle.classifyTriangle(a, b, c);
+		String expected = Triangle_Types.Scalene.toString();
+		String result = triangle.get_type().toString();
+		assertEquals(expected, result.toString());
+	}
+
+	@Test
+	public void check_Isosceles() {
+		int a = 3;
+		int b = 4;
+		int c = 4;
+		triangle.classifyTriangle(a, b, c);
+		String expected = Triangle_Types.isosceles.toString();
+		String result = triangle.get_type().toString();
+		assertEquals(expected, result.toString());
+	}
+}
